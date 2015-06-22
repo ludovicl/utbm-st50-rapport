c5e1f12814b41a447abd20bb37dd87c3bbeaafee
Ludovic Lardies <ludoviclrds@gmail.com>
2015-06-22 23:08:49 +0200

    add design pattern paragraph

---
+++ Ressources/Rapport_de_stage.mm
@@ -0,0 +1,95 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1432976713331" ID="ID_1631908572" MODIFIED="1432977124643" TEXT="Rapport de stage">
+<hook NAME="MapStyle">
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node CREATED="1432976719988" ID="ID_1219304239" MODIFIED="1432976725428" POSITION="right" TEXT="Pr&#xe9;sentation de l&apos;entreprise">
+<node CREATED="1432976737283" ID="ID_624634566" MODIFIED="1432976741930" TEXT="Domaines d&apos;activit&#xe9;s">
+<node CREATED="1433924948371" ID="ID_1371076565" MODIFIED="1433924951688" TEXT="Inventor-e"/>
+<node CREATED="1433924952293" ID="ID_1023017879" MODIFIED="1433924955380" TEXT="Arenametrix"/>
+</node>
+<node CREATED="1432976742864" ID="ID_550725503" MODIFIED="1432976750738" TEXT="Status juridique de l&apos;entreprise"/>
+</node>
+<node CREATED="1432976752178" ID="ID_531483680" MODIFIED="1434899686731" POSITION="left" TEXT="Le logiciel arenametrix">
+<node CREATED="1432976778915" ID="ID_624460349" MODIFIED="1432976787125" TEXT="Backend"/>
+<node CREATED="1432976788830" ID="ID_556894236" MODIFIED="1432976793290" TEXT="Frontend"/>
+<node CREATED="1432976794014" ID="ID_1738325477" MODIFIED="1432976803909" TEXT="Pr&#xe9;visions"/>
+<node CREATED="1432976805021" ID="ID_550344499" MODIFIED="1432976846398" TEXT="Le scoring"/>
+</node>
+<node CREATED="1432976834705" ID="ID_1055125570" MODIFIED="1434899464855" POSITION="right" TEXT="Mise en situation et description des activit&#xe9;s du stagiairre">
+<node CREATED="1432976866379" ID="ID_148634984" MODIFIED="1434899659606" TEXT="Python et flask pour le front">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1432976892943" ID="ID_939424025" MODIFIED="1434899662322" TEXT="Zurb fundation">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1432976878314" ID="ID_1699052835" MODIFIED="1434899643700" TEXT="Parsing des CSV en python">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1433925855077" ID="ID_724098" MODIFIED="1434899631590" TEXT="Design pattern strategy">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1432976944303" ID="ID_1839335042" MODIFIED="1432976960681" TEXT="Insertion dans la base de donn&#xe9;es avec le framework ruby on rails"/>
+<node CREATED="1432976974093" ID="ID_1223069482" MODIFIED="1432976976996" TEXT="Les API">
+<node CREATED="1432976978226" ID="ID_1504714329" MODIFIED="1432976981583" TEXT="Weezevent"/>
+<node CREATED="1432976982324" ID="ID_1628050805" MODIFIED="1432976986108" TEXT="Njuko"/>
+<node CREATED="1432977015780" ID="ID_972457891" MODIFIED="1432977016559" TEXT="sysv_ipc pour automatiser les imports"/>
+</node>
+<node CREATED="1432977040094" ID="ID_477955017" MODIFIED="1434899625831" TEXT="Rubinius pour am&#xe9;liorer les performances "/>
+<node CREATED="1432977074959" ID="ID_668803728" MODIFIED="1432977087933" TEXT="Scrapping de la pages Linkedin"/>
+<node CREATED="1432977095980" ID="ID_1353507961" MODIFIED="1432977110830" TEXT="Les base de donn&#xe9;es">
+<node CREATED="1432977111270" ID="ID_366624676" MODIFIED="1432977113886" TEXT="Arenapricing"/>
+<node CREATED="1432977115853" ID="ID_778544702" MODIFIED="1432977119344" TEXT="Arenapublic"/>
+<node CREATED="1433924897062" ID="ID_1748737024" MODIFIED="1433924923554" TEXT="Referencement des clients de Arenapricing dans Arenapublic"/>
+</node>
+<node CREATED="1433096089518" ID="ID_230226809" MODIFIED="1433096094748" TEXT="The Role"/>
+<node CREATED="1434899585177" ID="ID_693400789" MODIFIED="1434899608224" TEXT="Aggregation entre arenapricing et arenapublic"/>
+</node>
+</node>
+</map>
